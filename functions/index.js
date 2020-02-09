07260a59bf8c9549b3ef3b4a4223cbd89514cdb3
Amit Bourmad <amitburmad@gmail.com>
2020-02-09 17:30:30 +0200

    Added firebase functions

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+function setDispenserData(feed = null, feedAmount = null) {
+    let updateObj = { };
+
+    if (feedAmount) { Object.assign(updateObj, { feedAmount }) }
+    if (feed !== null) { Object.assign(updateObj, { feed }) }
+
+    return admin.database().ref('/Dispenser').update(updateObj)
+}
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.manualFeed = functions.https.onRequest(async (request, response) => {
+    await setDispenserData(true);
+    response.send("Feed request was sent");
+});
+
+exports.feedCatsSchedule = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    setDispenserData(true);
+});
